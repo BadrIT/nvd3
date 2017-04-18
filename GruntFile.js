34943c78c5f52b656e1f91ecc46ce51e60034963
Ahmed Amr <ahmed.amr@badrit.com>
2017-04-18 12:10:46 +0200

    Select core test case in GruntFile

--- GruntFile.js
+++ GruntFile.js
@@ -132,7 +132,7 @@ module.exports = function(grunt) {
                         'bower_components/d3/d3.js',
                         'src/*.js',
                         'src/models/*.js',
-                        'test/mocha/*.coffee'
+                        'test/mocha/core.coffee'
                     ],
                     exclude: [
                         'src/intro.js',
